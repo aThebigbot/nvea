87aed6462f6ad5c2151ed7e2489746069d323e99
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-03-08 19:17:25 +0100

    feat: change args

--- src/function/args.ts
+++ src/function/args.ts
@@ -1,8 +1,7 @@
 import process from "node:process";
 
-function args(callback:any) {
-     let res = [process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6], process.argv[7], process.argv[8], process.argv[9], process.argv[10]]
-     callback(res)
+function args(n: number) {
+     return process.argv[n];
 }
 
 export { args }
