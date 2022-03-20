d00dbaaf0ab3bf3397a046fc427a993cc2285449
Gabry-Pro <gabryuso88@gmail.com>
2022-03-20 15:33:44 +0100

    feat: aliases, callback fix, argv without undefined

--- src/function/command.ts
+++ src/function/command.ts
@@ -1,13 +1,20 @@
-import process from "node:process";
+import process from 'node:process';
 
-function command(name:string, callback:any) {
- if (process.argv[2] == name) {
-     let res = {
-         name: process.argv[2],
-         args: [process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6], process.argv[7], process.argv[8], process.argv[9], process.argv[10]]
-     }
-     callback(res)
- }
+async function command(name: string, aliases: String[], callback: any) {
+    if (process.argv[2] == name || process.argv[2] && aliases[0] && aliases.includes(process.argv[2])) {
+        let arr: String[] = []
+        for (let I: number = 0; I < process.argv.length; I++) {
+            if (I > 1) {
+                arr.push(process.argv[I])
+            }
+        }
+        let res = {
+            name: process.argv[2],
+            args: arr
+        }
+
+        await callback(res)
+    }
 }
 
 export { command }
